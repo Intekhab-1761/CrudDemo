2ab0f09beb8dc7f15c78bbb68b8f27380e3966a8
Intekhab Alam <intekhab1761@gmail.com>
2022-03-24 00:41:53 +0530

    Autowire

---
+++ SpringAutowired/src/main/java/com/tns/Springaw/Human.java
@@ -0,0 +1,22 @@
+package com.tns.Springaw;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Human {
+	public Human() {
+	}
+
+	Heart heart;
+
+	@Autowired
+	@Qualifier("humanHeart")
+	public void setHeart(Heart heart) {
+		this.heart = heart;
+	}
+
+	public void startPumping() {
+		heart.pump();
+	}
+
+}
